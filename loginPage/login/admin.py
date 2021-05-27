c08a70c0c5ed53307584fa85ff0843a9b56bc1ab
Soham Mandal <65412265+soham-mandal@users.noreply.github.com>
2021-05-27 20:51:33 +0530

    Add files via upload

---
+++ loginPage/login/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+
+#rewrite default adfmin page 
+class CustomUserAdmin(UserAdmin):
+    """Define admin model for custom User model with no username field."""
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
