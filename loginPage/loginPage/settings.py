ffb3e4786c3f6bf9e853a034e3c6e0ae779b34fa
Soham Mandal <65412265+soham-mandal@users.noreply.github.com>
2021-05-27 20:54:32 +0530

    Update settings.py

--- loginPage/loginPage/settings.py
+++ loginPage/loginPage/settings.py
@@ -115,9 +115,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'login.CustomUser'
